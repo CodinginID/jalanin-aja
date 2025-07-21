20e544405e0e9556edb5e1a7da76314092286e86
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2025-07-22 02:51:31 +0700

    update main.py

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ CORS(app)
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__" :
+    port = int(os.getenv('APP_PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
